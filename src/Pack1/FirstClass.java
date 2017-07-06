42f6467d9beeae1d45d60812abbb2c39102039eb
etroks <etroks@yandex.ru>
2017-07-06 10:56:21 +0300

    commit across inellij

--- src/Pack1/FirstClass.java
+++ src/Pack1/FirstClass.java
@@ -5,6 +5,6 @@ package Pack1;
  */
 public class FirstClass {
     public static void main(String[] args){
-        System.out.println("ky ky");
+        System.out.println("testing swap");
     }
 }
