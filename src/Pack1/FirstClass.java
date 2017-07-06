cefefe449f3af702059356b2a9408fff9a4da2a6
etroks <etroks@yandex.ru>
2017-07-06 10:39:48 +0300

    first version

---
+++ src/Pack1/FirstClass.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+/**
+ * Created by etrok on 06.07.2017.
+ */
+public class FirstClass {
+    public static void main(String[] args){
+        System.out.println("ky ky");
+    }
+}
